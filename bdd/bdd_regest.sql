da36049fd02995c8a2c979973ad1974505da1a5f
Manolyta <103241466+Manolyta@users.noreply.github.com>
2023-02-23 09:46:30 +0100

    update bdd

--- bdd/bdd_regest.sql
+++ bdd/bdd_regest.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql-galle-tessonneau.alwaysdata.net
--- Generation Time: Feb 21, 2023 at 07:25 PM
+-- Generation Time: Feb 22, 2023 at 05:54 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.19
 
@@ -53,13 +53,6 @@ CREATE TABLE `cr_reunion` (
   `id_projet` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `cr_reunion`
---
-
-INSERT INTO `cr_reunion` (`id_cr_reunion`, `nom`, `date_reunion`, `sujet`, `id_utilisateur`, `id_projet`) VALUES
-(17, 'logo_avant.png', '2023-02-21', 'Test', 1, 135);
-
 -- --------------------------------------------------------
 
 --
@@ -77,10 +70,7 @@ CREATE TABLE `equipe` (
 --
 
 INSERT INTO `equipe` (`id_equipe`, `nom`, `id_projet`) VALUES
-(1, 'Développement Regest', 1),
-(2, 'Dev carte JS', 2),
-(3, 'Equipe de jérémy rejoint projet fini', 7),
-(7, 'Test', 2);
+(1, 'Développement Regest', 1);
 
 -- --------------------------------------------------------
 
@@ -96,15 +86,6 @@ CREATE TABLE `fichier` (
   `id_repertoire` int(11) DEFAULT NULL,
   `id_projet` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `fichier`
---
-
-INSERT INTO `fichier` (`id_fichier`, `nom`, `date_ajout`, `id_utilisateur`, `id_repertoire`, `id_projet`) VALUES
-(1, 'Note de frais 1', '2022-11-25', 1, NULL, 1),
-(2, 'Note de frais 2', '2022-11-25', 1, NULL, 1),
-(3, 'Note de frais 3', '2022-11-25', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -167,18 +148,6 @@ CREATE TABLE `liste_to_do_list` (
   `id_projet` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `liste_to_do_list`
---
-
-INSERT INTO `liste_to_do_list` (`id_liste_to_do_list`, `nom`, `id_projet`) VALUES
-(2, 'Liste 2', 3),
-(19, 'Back-end', 1),
-(30, 'Fonctionnalité', 1),
-(31, 'Ajout supplémentaire (pas prioritaire)', 1),
-(32, 'Dev', 2),
-(34, 'dd', 1);
-
 -- --------------------------------------------------------
 
 --
@@ -205,9 +174,6 @@ INSERT INTO `news` (`id_news`, `titre`, `contenu`, `date`, `id_projet`, `id_util
 (9, 'Fin de l\'ajout des news ! Yay !', 'Nous avons enfin fini de développé l\'ajout des news !', '2022-12-05', 1, 1),
 (10, 'C\'est cassé ? ', 'Et voilà un bonne chose de faite aussi !', '2022-12-05', 1, 1),
 (12, 'Présentation suivi projet', 'Coucou les amis je suis sauvage', '2022-12-09', 1, 1),
-(15, 'qdsqdqs', 'dqsdqsd', '2023-02-03', 17, 1),
-(16, 'dsqdqsd', 'dsqdqsd', '2023-02-03', 17, 1),
-(17, 'C\'est cassé ? ', 'Régler le problème d\'heures', '2023-02-03', 2, 1),
 (22, 'Consigne oral de fin de période', 'Deux présentations orales :\r\n\r\n	Présentation académique :\r\n\r\n		Comment on a mener le projet ? \r\n		Les choix techniques ?\r\n\r\n	Présentation / Démonstration à un investisseur :\r\n\r\n		Démonstration : validation du point de vue\r\n		Element de création d\'entreprise ?\r\n		Remise de la documentation / manuel utilisateur ? \r\n	\r\nPossible redondance entre les 2 oraux.\r\n', '2023-02-07', 1, 1);
 
 -- --------------------------------------------------------
@@ -231,14 +197,7 @@ CREATE TABLE `projet` (
 --
 
 INSERT INTO `projet` (`id_projet`, `nom`, `date_debut`, `date_fin`, `status`, `id_chef_de_projet`, `id_site`) VALUES
-(1, 'Regest', '2022-12-09', '2023-04-26', 0, 1, 121),
-(2, 'Projet carte javascript', '2022-11-01', NULL, 0, 3, 1),
-(3, 'Projet 4D', '2022-11-02', NULL, 1, 1, 1),
-(7, 'Projet participe fini', '2022-12-08', NULL, 1, 3, 1),
-(17, 'nom_projet', '0000-00-00', '0000-00-00', 1, 1, 1),
-(131, 'aa', '2023-02-14', '2023-02-15', 0, 1, 121),
-(134, 'test', '2023-02-21', '2023-02-10', 0, 1, 6),
-(135, 'test3', '2023-02-21', '2023-02-24', 0, 1, 121);
+(1, 'Regest', '2022-12-09', '2023-04-26', 0, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -254,15 +213,6 @@ CREATE TABLE `repertoire` (
   `id_projet` int(11) NOT NULL,
   `id_repertoire_pere` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `repertoire`
---
-
-INSERT INTO `repertoire` (`id_repertoire`, `nom`, `date_ajout`, `id_utilisateur`, `id_projet`, `id_repertoire_pere`) VALUES
-(1, 'Dossier 1', '2022-11-01', 1, 1, NULL),
-(2, 'Dossier 2', '2022-11-01', 1, 1, NULL),
-(3, 'Sous-dossier 1', '2022-11-01', 1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -299,10 +249,7 @@ CREATE TABLE `site` (
 --
 
 INSERT INTO `site` (`id_site`, `nom`, `adresse`) VALUES
-(1, 'Groupama Supports & Services - Castelnau-le-Lez', '1000 Avenue Marcel Dassault\r\n34170 Castelnau-le-Lez'),
-(2, '108', 'test2'),
-(6, 'Site test aaaab', 'Adresse du site !'),
-(121, 'Testestss', 'jfsdjge');
+(1, 'Groupama Supports & Services - Castelnau-le-Lez', '1000 Avenue Marcel Dassault\r\n34170 Castelnau-le-Lez');
 
 -- --------------------------------------------------------
 
@@ -323,29 +270,6 @@ CREATE TABLE `tache_to_do_list` (
   `id_liste_to_do_list` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Dumping data for table `tache_to_do_list`
---
-
-INSERT INTO `tache_to_do_list` (`id_tache_to_do_list`, `nom`, `contenu`, `date_creation`, `date_debut`, `date_fin`, `echeance`, `etat`, `id_utilisateur`, `id_liste_to_do_list`) VALUES
-(3, 'Dev 4D', '', NULL, '2022-11-01', NULL, NULL, 0, 1, 2),
-(14, 'Gestion des sites', 'Ajout, modification, suppresion, dans MesProjets et accueilProjet', NULL, '2022-12-16', '2022-12-20', '2022-12-20', 3, 1, 19),
-(15, 'Ranger les vuex', '', NULL, '0000-00-00', NULL, '0000-00-00', 0, 1, 19),
-(16, 'Page Equipe', 'Gestion des équipe terminé !', NULL, '2023-01-02', '2023-01-05', '2023-01-05', 3, 1, 19),
-(17, 'Gestion de la page ToDO (ajouter, éditer, supprime', '', NULL, '2022-12-12', '2022-12-16', '2022-12-14', 3, 1, 19),
-(18, 'Gestion des dates', 'Mettre les current date dans les date de créa (projet, tache, news)', NULL, '2022-12-15', '2022-12-15', '2022-12-16', 3, 1, 19),
-(31, 'Redirection page d\'accueil', 'Redirection page d\'accueil', NULL, '0000-00-00', '0000-00-00', '0000-00-00', 3, 1, 19),
-(32, 'Gestion des droits', 'Si on est créateur du projet, on peut modifier, sinon non. Fonctionne', NULL, '2022-12-20', '2023-01-01', '2023-01-05', 3, 1, 19),
-(33, 'Mot de passe hash + salt', '', NULL, '2023-01-10', '2023-01-12', '2023-01-12', 3, 1, 19),
-(34, 'Messagerie', '', NULL, '2023-01-30', '1901-01-01', '1901-01-01', 2, 1, 30),
-(35, 'Gantt', '', NULL, '2023-01-30', NULL, '0001-01-01', 0, 1, 30),
-(36, 'Serveur de fichier', '', NULL, '2023-01-30', NULL, '0001-01-01', 0, 1, 30),
-(37, 'ON cascade BDD', '', NULL, '2023-01-30', NULL, '0000-00-00', 0, 1, 19),
-(39, 'Ajouter les chefs d\'équipes et les droits avec', '', NULL, '2023-01-24', NULL, '2023-02-26', 0, 1, 31),
-(40, 'Calendrier', '', NULL, '2023-01-30', NULL, '2023-03-05', 0, 1, 30),
-(41, '1', '1111', NULL, '2023-02-03', NULL, '0000-00-00', 0, 1, 32),
-(44, 'dd', 'd', NULL, '2023-02-17', NULL, '2023-02-18', 2, 1, 34);
-
 -- --------------------------------------------------------
 
 --
@@ -363,11 +287,7 @@ CREATE TABLE `user_rejoint_equipe` (
 --
 
 INSERT INTO `user_rejoint_equipe` (`date_entree`, `id_utilisateur`, `id_equipe`) VALUES
-(NULL, 1, 3),
 ('2023-01-05', 1, 1),
-('2023-02-02', 3, 7),
-('2023-02-02', 1, 7),
-('2023-02-02', 2, 2),
 ('2023-02-07', 5, 1),
 ('2023-02-17', 3, 1),
 ('2023-02-17', 2, 1);
@@ -387,6 +307,7 @@ CREATE TABLE `utilisateur` (
   `email` varchar(50) DEFAULT NULL,
   `poste` varchar(50) DEFAULT NULL,
   `service` varchar(50) DEFAULT NULL,
+  `nom_image` varchar(50) DEFAULT NULL,
   `lien_calendar` varchar(256) DEFAULT NULL,
   `id_role` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -395,14 +316,12 @@ CREATE TABLE `utilisateur` (
 -- Dumping data for table `utilisateur`
 --
 
-INSERT INTO `utilisateur` (`id_utilisateur`, `password`, `nom`, `prenom`, `telephone`, `email`, `poste`, `service`, `lien_calendar`, `id_role`) VALUES
-(1, '8d81994270a41fbf34beb634235075060fb873cf229aa68afd2d50e6dc7afe49', 'ANGELI', 'Manon', '0658259635', 'user', 'Développeur', 'Développement', 'https://calendar.google.com/calendar/u/0/embed?src=p2rn64sthgafnnpe0ug4p8fkds@group.calendar.google.com&ctz=Europe/Paris', 1),
-(2, '$2y$10$kVwHyN04Eoeqew0vsdJ2iOkDKEW2H/bSdwV9TU/ouZ3usEJ1E7ngi', 'limata', 'nicolas', '0782545752', 'nicolas.limata@mines-ales.org', 'comique', 'clown', NULL, 1),
-(3, 'ca41dd8b7e0a3e7d9aa7f8e689c48936d95d848d97febd6362c9cb60da275512', 'Angeli', 'Manon', '0618084480', 'manon', NULL, NULL, NULL, 1),
-(4, '09de0ab0f8dff491c36c0f09e5f33e6f52fe805bd2383fbf2db88f5e7df43a27', 'Locatelli', 'Julia', NULL, 'julia.locatelli@mines-ales.org', NULL, NULL, NULL, 1),
-(5, '2db9372c8e496073f53521b87a2118effeb074662bf4c78f5562d14de1b4f3d0', 'Regeste', 'Corky', NULL, 'corky', NULL, NULL, NULL, 1),
-(116, '3468f484613a6149d9000ea6c1d8ab899177bb8f515a061450fc55f65f2c9d47', 'a', 'a', NULL, 'a', NULL, NULL, NULL, 1),
-(117, '93254a349455c3ead8847f1c47aad2fa2c7190a1ad9d4ca3f28414f7a169820c', 'b', 'b', NULL, 'b', NULL, NULL, NULL, 1);
+INSERT INTO `utilisateur` (`id_utilisateur`, `password`, `nom`, `prenom`, `telephone`, `email`, `poste`, `service`, `nom_image`, `lien_calendar`, `id_role`) VALUES
+(1, '8d81994270a41fbf34beb634235075060fb873cf229aa68afd2d50e6dc7afe49', 'ANGELI', 'Manon', '0658259635', 'user', 'Développeur', 'Développement', 'image.png', 'https://calendar.google.com/calendar/u/0/embed?src=p2rn64sthgafnnpe0ug4p8fkds@group.calendar.google.com&ctz=Europe/Paris', 1),
+(2, '$2y$10$kVwHyN04Eoeqew0vsdJ2iOkDKEW2H/bSdwV9TU/ouZ3usEJ1E7ngi', 'limata', 'nicolas', '0782545752', 'nicolas.limata@mines-ales.org', 'comique', 'clown', NULL, NULL, 1),
+(3, 'ca41dd8b7e0a3e7d9aa7f8e689c48936d95d848d97febd6362c9cb60da275512', 'Angeli', 'Manon', '0618084480', 'manon', NULL, NULL, NULL, NULL, 1),
+(4, '09de0ab0f8dff491c36c0f09e5f33e6f52fe805bd2383fbf2db88f5e7df43a27', 'Locatelli', 'Julia', NULL, 'julia.locatelli@mines-ales.org', NULL, NULL, NULL, NULL, 1),
+(5, '2db9372c8e496073f53521b87a2118effeb074662bf4c78f5562d14de1b4f3d0', 'Regeste', 'Corky', NULL, 'corky', NULL, NULL, NULL, NULL, 1);
 
 --
 -- Indexes for dumped tables
@@ -539,7 +458,7 @@ ALTER TABLE `autorisation`
 -- AUTO_INCREMENT for table `cr_reunion`
 --
 ALTER TABLE `cr_reunion`
-  MODIFY `id_cr_reunion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_cr_reunion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `equipe`
@@ -551,7 +470,7 @@ ALTER TABLE `equipe`
 -- AUTO_INCREMENT for table `fichier`
 --
 ALTER TABLE `fichier`
-  MODIFY `id_fichier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_fichier` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `gantt_links`
@@ -581,7 +500,7 @@ ALTER TABLE `news`
 -- AUTO_INCREMENT for table `projet`
 --
 ALTER TABLE `projet`
-  MODIFY `id_projet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=136;
+  MODIFY `id_projet` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
 
 --
 -- AUTO_INCREMENT for table `repertoire`
